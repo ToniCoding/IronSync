c30ec8877a5f0788505c874a45f9b03fbbe435eb
ToniCoding <antoniovallejo2612@gmail.com>
2025-08-06 22:40:30 +0200

    chg(v1.1.1): Lower session notes limit

--- src/main/java/com/ironSync/config/AppConstants.java
+++ src/main/java/com/ironSync/config/AppConstants.java
@@ -5,7 +5,7 @@ public final class AppConstants {
     public static final String APP_DEFAULT_EXERCISES_DATA_PATH = "data/default_exercises.json";
     public static final String USER_WORKOUT_DATA_PATH = "user_data/user_workout.json";
     public static final String USER_CUSTOM_EXERCISES_DATA_PATH = "user_data/custom_exercises.json";
-    public static final int SESSION_NOTES_MAX_CHARACTERS = 500;
+    public static final int SESSION_NOTES_MAX_CHARACTERS = 250;
     public static final int SESSION_TITLE_MAX_CHARACTERS = 30;
 
     private AppConstants() {
